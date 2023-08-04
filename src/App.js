6c5842ac8458cd467262e1198e170b1cbb8244e4
MarcosYH <marcosdeimos18@gmail.com>
2023-08-05 00:28:50 +0200

    starting

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+function App() {
+  return (
+    <div className="App">
+       
+    </div>
+  );
+}
+
+export default App;
